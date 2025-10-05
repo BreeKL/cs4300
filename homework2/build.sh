170e232bc8da7856828f870ac193693e4e93496d
Brianne <leathbk1512@gmail.com>
2025-10-05 04:00:19 +0000

    Updated build.sh to create django superuser

--- homework2/build.sh
+++ homework2/build.sh
@@ -11,3 +11,14 @@ cd movie_theater_booking
 # Run Django management commands
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "
+import os
+from django.contrib.auth.models import User
+username = 'superuser'
+email = 'admin@example.com'
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+"
