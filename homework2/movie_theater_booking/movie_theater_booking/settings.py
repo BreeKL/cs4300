1c1e85ee022544701284d3f77d6f94db86d19052
Brianne <leathbk1512@gmail.com>
2025-10-05 03:41:39 +0000

    Updated .gitignore file

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ga!o$y7)$3xgp)*jjdbg)+527*s759v10#x58r^)m1mu+%8yt2'
+#SECRET_KEY = 'django-insecure-ga!o$y7)$3xgp)*jjdbg)+527*s759v10#x58r^)m1mu+%8yt2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
